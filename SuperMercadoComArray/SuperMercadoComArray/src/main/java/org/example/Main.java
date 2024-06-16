01937d7fec9c2d8a2762132c9a2a518d39d827f1
Luiz Henrique Sacramento <luizsacramento.tds@gmail.com>
2024-06-16 12:33:25 -0300

    projeto lista de supermercado com array concluido

---
+++ SuperMercadoComArray/SuperMercadoComArray/src/main/java/org/example/Main.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import implementation.SupermercadoArray;
+
+import java.util.Scanner;
+
+public class Main {
+    private final static int SIZE = 5;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        SupermercadoArray supermercadoArray = new SupermercadoArray(SIZE);
+        int opcao;
+        do{
+            System.out.println("\nLista de Compras");
+            System.out.println("1 - Inserir");
+            System.out.println("2 - Listar");
+            System.out.println("3 - Remover");
+            System.out.println("4 - Sair");
+            System.out.println("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao){
+                case 1:
+                    System.out.println("Digite o item a ser inserido: ");
+                    String item = scanner.next();
+                    supermercadoArray.add(item);
+                    break;
+                case 2:
+                    supermercadoArray.print();
+                    break;
+                case 3:
+                    System.out.println("Digite a posição do item a ser removido: ");
+                    int index = scanner.nextInt();
+                    supermercadoArray.delete(index);
+                    break;
+                case 4:
+                    System.out.println("Saindo do programa");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Por favor, escolha novamente.");
+            }
+        }while(opcao != 4);
+
+        scanner.close();
+    }
+}
